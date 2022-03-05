ce3d85d6ec46d1b7642cedd268ec6a9956d651b8
A00455338 <navdeep.kaur.navdeep.kaur@smu.ca>
2022-03-05 14:20:32 -0400

    Added Persist redux in the store

--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import App from './App.js';
 import { Provider }  from 'react-redux';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/lib/integration/react';
 import { configureStore } from './store';
 
+const store = configureStore();
+const persistor = persistStore(store);
+
 ReactDOM.render(
-    <Provider store={configureStore()}>
+    <Provider store={store}>
+        <PersistGate persistor={persistor} loading={<div>loading...</div>}>
         <App/>
+        </PersistGate>
     </Provider>,
     document.getElementById('root')
 );
--- src/store.js
+++ src/store.js
@@ -1,11 +1,21 @@
 import { createStore, combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 import {todos} from './todos/reducers';
 
 const reducers = {
     todos,
 };
 
-const rootReducers = combineReducers(reducers);
+const persistConfig ={
+    key:'root',
+    storage,
+    stateReconciler:autoMergeLevel2,
+}
 
-export const configureStore = () => createStore(rootReducers);
+const rootReducers = combineReducers(reducers);
+const persistedReducer = persistReducer(persistConfig,rootReducers);
+
+export const configureStore = () => createStore(persistedReducer);
 
