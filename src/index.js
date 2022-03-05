0b4af6cf0d92d0b3692fcd73ca525bdc51567292
A00455338 <navdeep.kaur.navdeep.kaur@smu.ca>
2022-03-05 11:13:05 -0400

    initial setup for redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import App from './App.js';
+import { Providers }  from 'react-redux';
+import { configureStore } from './store';
 
-ReactDOM.render(<App/>,document.getElementById('root'));
+ReactDOM.render(
+    <Providers store={configureStore()}>
+        <App/>
+    </Providers>,
+    document.getElementById('root')
+);
---
+++ src/store.js
@@ -0,0 +1,8 @@
+import { createStore, combineReducers } from 'redux';
+
+const reducers = {};
+
+const rootReducers = combineReducers(reducers);
+
+export const configureStore = () => createStore(rootReducers);
+
